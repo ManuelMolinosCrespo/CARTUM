ee4c967868037bfce43a2f0e39fa2a19dcc2d908
Saltimbanqui93 <mmolic00@estudiantes.unileon.es>
2015-05-03 14:57:26 +0200

    Arreglar ruteo css2

--- application/views/login.php
+++ application/views/login.php
@@ -8,14 +8,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<title>Login</title>
 	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"
       rel="stylesheet" type="text/css">
-      <link href="/css/style.css"
+      <link href="<?php echo base_url(); ?>css/style.css"
       rel="stylesheet" type="text/css">
       <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no" />
 </head>
 <body>
 	<div class="row">
 		<div class="col-md-1 no-float img-responsive">
-			<img src="img/ULE_Logo.png">
+			<img src="<?php echo base_url(); ?>img/ULE_Logo.png">
 		</div>
 	</div>
 	<div class="container">
