5cfac46aea98591c37726e386c98477ff53b8cbc
saltimbanqui93 <manuelmolinoscrespo@gmail.com>
2015-05-03 13:15:49 +0200

    Add EditProfile

---
+++ application/views/editProfile.php
@@ -0,0 +1,74 @@
+
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<title>Editar Perfil</title>
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"
+      rel="stylesheet" type="text/css">
+      <link href="css/style.css"
+      rel="stylesheet" type="text/css">
+      <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no" />
+</head>
+<body>
+	<div class="row">
+		<div class="col-md-1 no-float img-responsive">
+			<img src="img/ULE_Logo.png">
+		</div>
+	</div>
+	<div class="container">
+		<div class="col-md-5 col-xs-4 col-xs-offset-2 col-md-offset-5">
+			<?php
+				$correo = array(
+					'name' => 'correo',
+					'placeholder' => 'Ingresa tu correo electrónico'
+				);
+				$password = array(
+					'name' => 'password',
+					'placeholder' => 'Ingresa tu contraseña'
+				);
+				$passCheck = array(
+					'name' => 'repeat_password',
+					'placeholder' => 'Repite la contraseña'
+				);
+				$telefono = array(
+					'name' => 'telefono',
+					'placeholder' => 'Ingresa tu número de teléfono'
+				);
+			?>
+			<?= form_open("/editProfile_controller/recibirdatos") ?>
+			<div class="form-group">
+				<?= form_label('Correo Electrónico', 'correo')?>
+				<?= form_input($correo)?>
+			</div>
+			<br>
+			<div class="form-group">
+				<?= form_label('Contraseña', 'contraseña')?>
+				<?= form_password($password)?>
+			</div>
+			<br>
+			<div class="form-group">
+				<?= form_label('Confirmar Contraseña', 'confirmar_contraseña')?>
+				<?= form_password($passCheck)?>
+			</div>
+			<br>
+			<div class="form-group">
+				<?= form_label('Teléfono', 'telefono')?>
+				<?= form_input($telefono)?>
+			</div>
+			<div class="form-group">
+				<?= form_label('Foto', 'foto')?>
+				<?= form_upload('foto')?>		
+			</div>
+			<br>
+			<div class='btn btn-default button-confirm col-md-offset-2 col-xs-offset-2 color-letter'>
+				<?= form_submit('', 'Modificar') ?>
+				<?= form_close() ?> 
+			</div>
+		</div>
+	</div>
+</body>
+</html>
