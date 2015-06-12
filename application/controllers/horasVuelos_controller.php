7c944346444b65f2a38fef49a03c48d4307c8c56
saltimbanqui93 <manuelmolinoscrespo@gmail.com>
2015-06-12 12:25:22 +0200

    .

--- application/controllers/horasVuelos_controller.php
+++ application/controllers/horasVuelos_controller.php
@@ -9,6 +9,8 @@ class HorasVuelos_controller extends CI_Controller {
 		
 		parent::__construct();
 		 $this->load->model('horasVuelos_model');
+		 $this->load->model('fichaDron_model');
+		 $this->session->set_userdata('idDron', $this->uri->segment(3));
 	}
 
 	public function index() {
@@ -16,12 +18,22 @@ class HorasVuelos_controller extends CI_Controller {
 		$this->session->set_userdata('idDronHorasVuelos', $this->uri->segment(3));
 	}
 
+	public function obtenerdatos($id) {	
+
+		//Llamamos al modelo 
+	 	$data['datos'] = $this->fichaDron_model->obtenerFicha($id);
+		
+		$this->load->view('fichaDron',$data);
+	}
+
 	public function recibirDatos(){
+		$id = $this->session->userdata('idDron');
 		$datos = array(
 	 		'idDron' => $id = $this->session->userdata('idDronHorasVuelos'),
 	 		'horas' => $this->input->post('horas_vuelo')
 	 		);
 		$this->horasVuelos_model->incrementarHorasVuelo($datos);
+		$this->obtenerdatos($id);
 	}
 
 
